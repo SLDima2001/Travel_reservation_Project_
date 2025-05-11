5463cf411c83693f0fadc1f7a9d03a2c1630be43
Dimalsha Praveen <dimalshapraveen2001@gmail.com>
2025-05-11 14:21:09 +0530

    Update Days6.jsx

--- frontend/pages/Days6.jsx
+++ frontend/pages/Days6.jsx
@@ -41,7 +41,7 @@ function Days6() {
 
 
   const bookNow = () => {
-    navigate("/BookingForm"); // Change "/booking" to your target booking page route
+    navigate("/signin"); // Change "/booking" to your target booking page route
   };
 
 
